a56f8ef5620c1eb384ff3fc311d54602359888ae
Sara-Elena-Saji <elenasaji1997@gmail.com>
2026-04-15 23:24:50 +0530

    Added metadata storage using JSON and display original filenames

--- app.py
+++ app.py
@@ -1,26 +1,50 @@
 from flask import Flask, render_template, request
 import os
 import uuid
+import json
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
+DATA_FILE = "files.json"
+
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+def load_files():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, "r") as f:
+        return json.load(f)
+
+
+def save_files(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    files_data = load_files()
+
     if request.method == "POST":
         file = request.files["file"]
 
         if file:
             token = str(uuid.uuid4())
             stored_name = token + "_" + file.filename
+
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], stored_name))
 
-    # Get list of files
-    files = os.listdir(app.config["UPLOAD_FOLDER"])
+            # Save metadata
+            files_data.append({
+                "original_name": file.filename,
+                "stored_name": stored_name
+            })
 
-    return render_template("index.html", files=files)
+            save_files(files_data)
+
+    return render_template("index.html", files=files_data)
 
 
 if __name__ == "__main__":
