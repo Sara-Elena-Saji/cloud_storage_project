50a11bef80bc3aea11d6a7fe8cd5ebcc67212ef9
Sara-Elena-Saji <elenasaji1997@gmail.com>
2026-04-15 23:20:37 +0530

    Display uploaded files on UI

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import uuid
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+@app.route("/", methods=["GET", "POST"])
 def home():
-  return render_template("index.html")
+    if request.method == "POST":
+        file = request.files["file"]
+
+        if file:
+            token = str(uuid.uuid4())
+            stored_name = token + "_" + file.filename
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], stored_name))
+
+    # Get list of files
+    files = os.listdir(app.config["UPLOAD_FOLDER"])
+
+    return render_template("index.html", files=files)
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
